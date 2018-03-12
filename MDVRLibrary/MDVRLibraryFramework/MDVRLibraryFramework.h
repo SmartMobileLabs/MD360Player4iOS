003915ba79c1ebf12c7d4fbb6e834bf95f714cbc
mfedler <m.fedler@weptun.de>
2018-03-12 13:13:58 +0100

    Added framework target

---
+++ MDVRLibrary/MDVRLibraryFramework/MDVRLibraryFramework.h
@@ -0,0 +1,19 @@
+//
+//  MDVRLibraryFramework.h
+//  MDVRLibraryFramework
+//
+//  Created by Michael Fedler on 12.03.18.
+//  Copyright © 2018 asha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDVRLibraryFramework.
+FOUNDATION_EXPORT double MDVRLibraryFrameworkVersionNumber;
+
+//! Project version string for MDVRLibraryFramework.
+FOUNDATION_EXPORT const unsigned char MDVRLibraryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDVRLibraryFramework/PublicHeader.h>
+
+#import <MD360Player4iOS/MDVRLibrary.h>
